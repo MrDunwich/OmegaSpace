7b3959bc7ab019d81f37286df901d746a74ec535
MrDunwich <paulfsmith1607@gmail.com>
2022-09-19 14:19:17 +0900

    updated pause menu

--- Assets/UI/Menu System/PauseMenu.cs
+++ Assets/UI/Menu System/PauseMenu.cs
@@ -39,7 +39,7 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Main Menu");
         Time.timeScale = 1;
     }
 
